cf9e5b9744e447e1869b7f81d2dae91c0bf35626
ninhnguyen375 <ninhnguyen375@gmail.com>
2019-01-05 17:41:03 +0700

    game caro

---
+++ caro-game/precache-manifest.a33f663753d771b835973e0b24170f15.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3f4a1ef6b3b043ee879c",
+    "url": "/static/js/main.3f4a1ef6.chunk.js"
+  },
+  {
+    "revision": "abd9e857442f04b96f9b",
+    "url": "/static/js/1.abd9e857.chunk.js"
+  },
+  {
+    "revision": "3f4a1ef6b3b043ee879c",
+    "url": "/static/css/main.c456a611.chunk.css"
+  },
+  {
+    "revision": "349d7278c68bdd190a873fa5053b5224",
+    "url": "/index.html"
+  }
+];
